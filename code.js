942e62fbac8937750c58961553c97f2f2e83ac5f
Kaéna <114762014+kaena777@users.noreply.github.com>
2023-04-29 19:24:48 +0200

    Add files via upload

---
+++ code.js
@@ -0,0 +1,33 @@
+function toggleText() {
+  var text = document.getElementById("appear");
+  var bouton = document.getElementById('bouton');
+  if (text.style.display === "none") {
+    text.style.display = "block";
+    bouton.style.display = "none";
+  } else {
+    text.style.display = "none";
+    bouton.style.display = "block";
+  }
+}
+function toggleText1() {
+  var text = document.getElementById("appear1");
+  var bouton = document.getElementById('bouton1');
+  if (text.style.display === "none") {
+    text.style.display = "block";
+    bouton.style.display = "none";
+  } else {
+    text.style.display = "none";
+    bouton.style.display = "block";
+  }
+}
+function toggleText2() {
+  var text = document.getElementById("appear2");
+  var bouton = document.getElementById('bouton2');
+  if (text.style.display === "none") {
+    text.style.display = "block";
+    bouton.style.display = "none";
+  } else {
+    text.style.display = "none";
+    bouton.style.display = "block";
+  }
+}
